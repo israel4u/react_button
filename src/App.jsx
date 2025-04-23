7b25872b997ba173a665f4208ca71ac85c688bb9
Israel4u <israeloluwasholao@gmail.com>
2025-04-23 12:32:47 +0100

    Going through the React basics; working with button controlled differently.

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Button from './components/MyButton'
+
+function App() {
+ 
+
+  return (
+    <div className="wrapper">
+      <h1>Buttons Controlled Separately</h1>
+      <div className="container">
+        <div className="buttonOne"><Button /></div>
+        <div className="buttonTwo"><Button /></div>
+      </div>
+     
+    </div>
+  )
+}
+
+export default App
